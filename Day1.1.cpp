687b2689b2b55ad5e21912eafd69d97c45a8c570
Kunal <kunalkeshri047@gmail.com>
2021-05-25 19:15:11 +0530

    Added Day1 code 1

---
+++ Day1.1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
